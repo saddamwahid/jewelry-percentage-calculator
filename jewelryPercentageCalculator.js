5c496fe115c063d29ad9d0115ebfa328ae9f8397
wahidremon <wahidremon@gmail.com>
2024-12-05 16:19:57 +0600

    update 1

---
+++ jewelryPercentageCalculator.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//   var x = document.getElementById("myNumber").value;
+//   document.getElementById("demo").innerHTML = x;
+// }
+
+
+function percentage(num, per)
+{
+  return (num/100)*per;
+}
+          
+console.log(percentage(1000, 47.12));
